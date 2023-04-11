fc0d30f02062b7f9e7b2f8635dfc42e0a1d7087a
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-04-11 12:51:07 +0000

    Initial commit

---
+++ src/main/java/com/example/projet_pfa/ProjetPfaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_pfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetPfaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_pfa/ProjetPfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_pfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

31df891bb0bd41c4e569d0533e41c12a7ba60fee
omaima-aitzaouit <83766274+omaima-aitzaouit@users.noreply.github.com>
2023-04-18 01:15:16 +0000

    Controller

---
+++ src/main/java/com/example/projet_pfa/controller/SerieController.java
@@ -0,0 +1,50 @@
+package com.example.projet_pfa.controller;
+
+
+import com.example.projet_pfa.entity.Serie;
+
+import com.example.projet_pfa.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/series")
+public class SerieController {
+
+    @Autowired
+    private SerieService serieService;
+
+    @PostMapping("/")
+    public Serie save(@RequestBody Serie serie) { return serieService.save(serie); }
+
+    @GetMapping("/")
+    public List<Serie> findAll() { return serieService.findAll();}
+
+    @GetMapping("/{id}")
+    public Serie findById(@PathVariable int id) {return serieService.findById(id);}
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Integer id,@RequestBody Serie serie) {serieService.update(id, serie);}
+
+    @DeleteMapping("/")
+    public void deleteSerie(@PathVariable Integer id) {
+        serieService.deleteSerie(id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/projet_pfa/controller/SpecialiteController.java
@@ -0,0 +1,53 @@
+package com.example.projet_pfa.controller;
+
+
+import com.example.projet_pfa.entity.Serie;
+import com.example.projet_pfa.entity.Specialite;
+import com.example.projet_pfa.service.SpecialiteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/specialites")
+public class SpecialiteController {
+
+    @Autowired
+    private SpecialiteService specialiteService;
+
+
+    @PostMapping("/")
+    public Specialite save(@RequestBody Specialite specialite) { return specialiteService.save(specialite); }
+
+    @GetMapping("/")
+    public List<Specialite> findAll() { return specialiteService.findAll();}
+
+    @GetMapping("/{id}")
+    public Specialite findById(@PathVariable int id) {return specialiteService.findById(id);}
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Integer id,@RequestBody Specialite specialite) {specialiteService.update(id, specialite);}
+
+    @DeleteMapping("/")
+    public void deleteSpecialite(@PathVariable Integer id) {
+        specialiteService.deleteSpecialite(id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
